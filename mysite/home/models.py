66afdce215bb7210accf70eed32d52525eb8a27e
Ngahu <jamaalaraheem@gmail.com>
2017-06-29 01:11:29 +0300

    this is th creating of the first models that are going to be used for development purposes

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -2,3 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class Post(models.Model):
+    """
+    this is incharge of the post form
+    """
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True,auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False,auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
