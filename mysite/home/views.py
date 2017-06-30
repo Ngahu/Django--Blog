900a1e9fb13dd388fd7c2c9b4227b00785c631a1
Ngahu <jamaalaraheem@gmail.com>
2017-06-30 11:40:12 +0300

    maing a relative import of the djanf=gos messages frmework that will be used to show users when creating or updating the form

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
-
+from django.contrib import messages
 from . models import Post
 from. forms import PostForm
 
